0bb300f6160c0ec38b61fc76dcd279c241d34456
Mohammad Arif <arif_iq@yahoo.co.in>
2022-07-18 13:40:44 +0530

    Add client folder with all the required dependencies for UI

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+
+function App () {
+  return (
+    <>
+      <Header />
+      <div className='container'>
+        <h1>Hello</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
