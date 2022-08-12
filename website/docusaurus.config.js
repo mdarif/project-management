b54af3f66f45222a54bb07891d6fb78ce9fc0424
Mohammad Arif <arif_iq@yahoo.co.in>
2022-08-12 08:47:40 +0530

    More changes

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -14,7 +14,6 @@ const config = {
   // baseUrl: '/',
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
   deploymentBranch: 'gh-pages',
 
   // GitHub pages deployment config.
